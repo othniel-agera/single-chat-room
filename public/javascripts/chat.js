d921ea8abf1ac19959fd10b0162e4b4ba8d110a0
chisom <chisom.ofoedu@flutterwavego.com>
2023-02-16 11:35:12 +0100

    added functions for chat form

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,1 +1,40 @@
 const socket = io();
+
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const input = document.getElementById("input");
+
+socket.on("message", (message) => {
+  outputMessage(message);
+
+  //scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//submit message
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+
+  socket.emit("chatMessage", msg);
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+//output message to DOM
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = ` <p class="meta">user <span>6:00</span></p>
+  <p class="text">${message}</p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
+
+document.getElementById("leave-btn").addEventListener("click", () => {
+  const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
+  if (leaveRoom) {
+    window.location.href = window.location.href;
+  } else {
+  }
+});
