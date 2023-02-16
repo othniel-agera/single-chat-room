0a1d15d5443f65d126f146c9da8bc11118dd6fb2
chisom <chisom.ofoedu@flutterwavego.com>
2023-02-16 11:38:23 +0100

    corrected mongoose spelling

--- models/message.model.js
+++ models/message.model.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const { Schema } = mongose;
+const { Schema } = mongoose;
 
 const msgSchema = new Schema({
-	message: { type: String, required: true },
-	timeSent: { type: Date, default: Date.now },
-	sender: { type: String, required: true },
+  message: { type: String, required: true },
+  timeSent: { type: Date, default: Date.now },
+  sender: { type: String, required: true },
 });
 
 module.exports = mongoose.model("message", msgSchema);
