dcfb863548d8e11af15316fb1328919143eea65b
othniel-agera <othniel.agera@flutterwavego.com>
2023-02-21 16:05:47 +0100

    Get messages

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -14,6 +14,23 @@ const outputStatus = (statusInfo) => {
   <p class="text">${statusInfo.online ? "Online" : "Offline"}</p>`;
 	document.querySelector(".chat-messages").appendChild(div);
 };
+
+// Show loading before adding more messages
+const outputLoading = async (parent) => {
+	const div = document.createElement("div");
+	div.classList.add("loading");
+	const loadingPara = document.createElement("p");
+	loadingPara.innerHTML = "loading...";
+	div.appendChild(loadingPara);
+	parent.prepend(div);
+	return new Promise((resolve) =>
+		setTimeout(() => {
+			parent.removeChild(div);
+			resolve();
+		}, 2500)
+	);
+};
+
 //output message to DOM
 const outputMessage = (message) => {
 	const div = document.createElement("div");
@@ -26,7 +43,27 @@ const outputMessage = (message) => {
 		</p>
 		<p class="text">${message.messageText}</p>
 	`;
-	document.querySelector(".chat-messages").appendChild(div);
+	document.querySelector(".chat-messages").prepend(div);
+};
+
+// output btn to show more messages
+const outputBtn = (page) => {
+	const div = document.createElement("div");
+	div.classList.add("show-more");
+	const btn = document.createElement("button");
+	btn.innerHTML = "Show More";
+	div.appendChild(btn);
+	document.querySelector(".chat-messages").prepend(div);
+
+	btn.addEventListener("click", async () => {
+		const parent = document.querySelector(".chat-messages");
+		parent.removeChild(div);
+		await outputLoading(parent);
+		const msg = await getMessages(page);
+		if (msg.data.success) {
+			displayMessages(msg.data.data, msg.data.pagination);
+		}
+	});
 };
 
 // Show List of users
@@ -51,6 +88,7 @@ const showUserListStatus = (user) => {
 	}
 };
 
+// Send request to send message
 const postMessage = async (msgText) => {
 	try {
 		const msg = await axios.post(`/api/v1/messages`, {
@@ -63,6 +101,28 @@ const postMessage = async (msgText) => {
 	}
 };
 
+// Get first 5 messages
+const getMessages = async (page) => {
+	try {
+		const msg = await axios.get(`/api/v1/messages?page=${page}&limit=5`);
+		return msg;
+	} catch (err) {
+		throw new Error(err);
+	}
+};
+
+// Display the messages received
+const displayMessages = (msg, pagination) => {
+	for (let i = 0; i < msg.length; i++) {
+		outputMessage(msg[i]);
+	}
+	if (pagination?.next) {
+		outputBtn(pagination.next.page);
+	}
+	chatMessages.scrollTop = chatMessages.scrollHeight;
+};
+
+// Helper function to get time only
 const getTimeOnly = (dateObj) => {
 	const date = new Date(dateObj);
 	return `
@@ -74,6 +134,10 @@ const getTimeOnly = (dateObj) => {
 
 (async () => {
 	const { data } = await axios.get(`/api/v1/auth/me`);
+	const msg = await getMessages(1);
+	if (msg.data.success) {
+		displayMessages(msg.data.data, msg.data.pagination);
+	}
 
 	socket.on("status", (info) => {
 		const registerSend = {
--- utils/advancedResults.util.js
+++ utils/advancedResults.util.js
@@ -1,80 +1,69 @@
-const advancedResults = async (
-  model,
-  filter,
-  options,
-) => {
-  const {
-    limit = 25,
-    page = 1,
-    populate,
-    select,
-    sort,
-    distinct,
-  } = options;
-  let query;
+const advancedResults = async (model, filter, options) => {
+	const { limit = 25, page = 1, populate, select, sort, distinct } = options;
+	let query;
 
-  // Create query string
-  let queryStr = JSON.stringify(filter);
+	// Create query string
+	let queryStr = JSON.stringify(filter);
 
-  // Create operators ( $gt, $gte, etc)
-  queryStr = queryStr.replace(
-    /\b(gt|gte|lt|lte|in)\b/g,
-    (match) => `$${match}`,
-  );
+	// Create operators ( $gt, $gte, etc)
+	queryStr = queryStr.replace(
+		/\b(gt|gte|lt|lte|in)\b/g,
+		(match) => `$${match}`
+	);
 
-  // Finding resource
-  query = model.find(JSON.parse(queryStr));
+	// Finding resource
+	query = model.find(JSON.parse(queryStr));
 
-  // Select fields
-  if (select) {
-    const fields = select.split(',').join(' ');
-    query = query.select(fields);
-  }
+	// Select fields
+	if (select) {
+		const fields = select.split(",").join(" ");
+		query = query.select(fields);
+	}
 
-  // Sort
-  if (sort) {
-    const sortBy = sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createdAt');
-  }
+	// Sort
+	if (sort) {
+		const sortBy = sort.split(",").join(" ");
+		query = query.sort(sortBy);
+	} else {
+		query = query.sort("-createdAt");
+	}
 
-  // Pagination
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await model.countDocuments(JSON.parse(queryStr));
-  query = query.skip(startIndex).limit(limit);
+	// Pagination
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	const total = await model.countDocuments(JSON.parse(queryStr));
+	query = query.skip(startIndex).limit(limit);
 
-  if (distinct) {
-    query = query.distinct(distinct);
-  }
-  if (populate) {
-    query = query.populate(populate);
-  }
+	if (distinct) {
+		query = query.distinct(distinct);
+	}
+	if (populate) {
+		query = query.populate(populate);
+	}
 
-  const results = await query.exec();
+	const results = await query.exec();
 
-  // Pagination result
-  const pagination = {};
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit,
-    };
-  }
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit,
-    };
-  }
-  const totalCount = await model.find(JSON.parse(queryStr));
-  return {
-    totalCount: totalCount.length,
-    countOnPage: results.length,
-    pagination,
-    data: results,
-  };
+	// Pagination result
+	const pagination = {};
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit,
+		};
+	}
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit,
+		};
+	}
+	const totalCount = await model.find(JSON.parse(queryStr));
+	return {
+		totalCount: totalCount.length,
+		countOnPage: results.length,
+		pagination,
+		data: results,
+	};
 };
 
 module.exports = advancedResults;
